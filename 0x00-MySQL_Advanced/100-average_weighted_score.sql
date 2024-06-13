c59c5882f0bb83c1624fad5bf6c3007e21c41038
mostafazhrn <mostafazhrn@gmail.com>
2024-06-13 18:41:57 -0400

    push_task12_fix1

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,13 +1,16 @@
--- this script shall create a stored procedure ComputeAverag...
+-- this script shall create a stored procedure ComputeAverageWeighted...
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
-BEGIN
-    DECLARE weighted_score FLOAT;
-    SET weighted_score = (SELECT SUM(score * weight) / SUM(weight)
-    FROM users AS U JOIN corrections as C ON U.id = C.user_id
-    JOIN projects AS P ON C.project_id = P.id
-    WHERE U.id = user_id);
-UPDATE users SET average_weighted_score = weighted_score WHERE id = user_id;
-END//
-DELIMITER ;
+BEGIN DECLARE w_avg_score FLOAT;
+SET w_avg_score = (
+SELECT SUM(score * weight) / SUM(weight)
+FROM users AS U
+JOIN corrections AS C
+ON U.id = C.user_id
+JOIN projects AS P
+ON C.project_id = P.id
+WHERE U.id = user_id);
+UPDATE users SET average_score = w_avg_score WHERE id = user_id;
+END //
+DELIMITER;
