bc25c83270caf6ba692167af0a399c781dfe972e
mostafazhrn <mostafazhrn@gmail.com>
2024-06-19 14:12:28 -0400

    push_task5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" this script shall obtain an html content from a webpage """
+import requests
+import redis
+x= redis.Redis()
+compte = 0
+
+
+def get_page(url: str) -> str:
+    """ This shall fetch the html content of a webpage """
+    x.set(f"cached:{url}", count)
+    rspo = requests.get(url)
+    x.incr(f"count:{url}")
+    x.setex(f"cached:{url}", 10, x.get(f"count:{url}"))
+    return rspo.text
